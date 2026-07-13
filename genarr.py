91c8719ff0c537d5b6bfebe4b891c7ae1e33cbad
Shyam <196654975+SamJ1005@users.noreply.github.com>
2026-07-13 10:49:23 +0530

    Refactor array operations in genarr.py
    Updated array generation and stacking methods for clarity and precision.

--- genarr.py
+++ genarr.py
@@ -1,29 +1,22 @@
 import numpy as np
 
 # 2(a). Create 10 evenly spaced values between 0 and 1
-
-arr = np.linspace(0,1,10)
+arr=np.round(np.linspace(0,1,10),3)
 print(arr)
 
 # 2(b). Generate a 3x3 Identity Matrix
-
 arr = np.eye(3)
 print(arr)
 
 # 2(c). Reshape a 1D array into (2x5)
-
 arr = np.array(list(map(int, input("Enter 10 elements: ").split())))
 
 arr = arr.reshape(2,5)
 print(arr)
 
 # 2(d). Stack Two Arrays
+a=np.array(list(map(int, input("Enter 3 elements for array A: ").split())))
+b=np.array(list(map(int, input("Enter 3 elements for array B: ").split())))
 
-a = np.array(list(map(int, input("Enter first array (3 elements): ").split())))
-b = np.array(list(map(int, input("Enter second array (3 elements): ").split())))
-
-print("Vertical Stack")
-print(np.vstack((a,b)))
-
-print("Horizontal Stack")
-print(np.hstack((a,b)))
+print("Vertical Stack: \n",np.vstack((a,b)))
+print("Horizontal Stack: \n",np.hstack((a,b)))
