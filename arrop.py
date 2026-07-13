a5c86254785febcce73c1f9d5a8329a4b2de5488
Shyam <196654975+SamJ1005@users.noreply.github.com>
2026-07-13 10:47:47 +0530

    added round function

--- arrop.py
+++ arrop.py
@@ -7,7 +7,7 @@ print(arr)
 
 # 1(b). Create a 2D array (3x4) with random numbers
 
-arr = np.random.rand(3,4)
+arr=np.round(np.random.rand(3,4),3)
 print(arr)
 
 # 1(c). Calculate Mean and Standard Deviation
@@ -16,15 +16,11 @@ arr = np.array(list(map(int, input("Enter array elements: ").split())))
 
 print("Array:", arr)
 print("Mean:", np.mean(arr))
-print("Standard Deviation:", np.std(arr))
+print("Standard Deviation: ", np.round(np.std(arr)),4)
 
 # 1(d). Normalize the Array
-
-arr = np.array(list(map(int, input("Enter array elements: ").split())))
-
 mean = np.mean(arr)
 std = np.std(arr)
-
 normal = (arr - mean) / std
 
-print("Normalized Array:", normal)
+print("\nNormalized Array: ",np.round(normal,3))
