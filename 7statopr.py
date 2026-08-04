8965c121bd176111c59efda42e8acc0fd02a0bb9
SamJ1005 <196654975+SamJ1005@users.noreply.github.com>
2026-08-04 23:11:31 +0530

    Updated specific values

--- 7statopr.py
+++ 7statopr.py
@@ -1,24 +1,20 @@
 import pandas as pd
 
-n = int(input("Enter the number of employees: "))
+n = int(input("Enter number of employees: "))
 
 data = []
 
 for i in range(n):
-    print("\nEnter details of Employee", i + 1)
+    salary = int(input(f"Enter salary of Employee {i+1} : "))
 
-    empid = int(input("Employee ID : "))
-    name = input("Employee Name : ")
-    salary = int(input("Salary : "))
+    data.append([salary])
 
-    data.append([empid, name, salary])
-
-df = pd.DataFrame(data, columns=["Employee ID", "Employee Name", "Salary"])
+df = pd.DataFrame(data, columns=["Salary"])
 
 print("\nDataset:\n",df)
 
 print("\nMean Salary:", df["Salary"].mean())
 print("Median Salary:", df["Salary"].median())
 print("Mode Salary:\n",df["Salary"].mode())
-print("Variance:", df["Salary"].var())
+print("\nVariance:", df["Salary"].var())
 print("Standard Deviation:", df["Salary"].std())
